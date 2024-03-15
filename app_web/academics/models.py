3522ac2b371e657d667416ae3f6427b18d573874
Brayan Lagos <camilolagos5670@gmail.com>
2024-03-15 17:17:07 -0500

    Actualizacion

--- app_web/academics/models.py
+++ app_web/academics/models.py
@@ -2,8 +2,19 @@ import datetime
 from django.db import models
 
 # Create your models here.
+#model.DateTimeField(auto_now = true, null = false ) => update_at
+#model.DateTimeField(auto_now_add = true, null = false ) => Defaul now()
 #usamos models.Model para usar los timpos de campos ejemplos variables
 class User(models.Model):
+    #como puedo factorizar los datos en una sola clase 
+    email = models.EmailField(null = True, blank = True)
+    password = models.CharField(null = True, blank = True)
+    status = models.BooleanField(null = True, blank = True, default = True)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+class Person(models.Model):
     firstname = models.CharField(max_length = 20) 
     lastname = models.CharField(max_length = 20)
     age = models.IntegerField()
@@ -11,3 +22,4 @@ class User(models.Model):
     created_at = models.DateTimeField(default = datetime.datetime.now())
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
+
